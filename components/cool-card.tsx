9bf4fce048978388695f784b1784935be3b5b0a2
modernwallpaper <j.cunz@grimmels.de>
2024-06-02 15:44:12 +0200

    Updated Cards

--- components/cool-card.tsx
+++ components/cool-card.tsx
@@ -3,12 +3,14 @@ import { cn } from "@/lib/utils"
 import "./poly-card.css"
 import { GeistMono } from "geist/font/mono"
 import { useRouter } from "next/navigation"
+import { ArrowRight } from "lucide-react"
 
 export const PolyCard = ({ header, children }: { children: React.ReactNode, header: string }) => {
   const router = useRouter()
 
   return(
-    <div className="p-7 w-60 min-w-48 grid cursor-pointer poly-card backdrop-blur bg-neutral-900 hover:bg-neutral-800 transition-colors"
+    <div className="relative flex-col p-7 w-60 min-w-48 grid cursor-pointer poly-card 
+      backdrop-blur bg-neutral-900 hover:bg-neutral-800 hover:text-white transition-colors"
       onClick={() => {
         router.push("/"+header)
       }}
@@ -25,6 +27,9 @@ export const PolyCard = ({ header, children }: { children: React.ReactNode, head
           {children}
         </div>
       </div>
+      <p className="absolute w-full h-full flex justify-end items-start p-7 text-muted-foreground hover:text-white transition-colors">
+        <ArrowRight className="w-5 h-5"/>
+      </p>
     </div>
   )
 }
