f3745f2fbbbfc6eb7d10eaa32b00954c9c158258
modernwallpaper <j.cunz@grimmels.de>
2024-06-02 19:28:09 +0200

    batman

--- components/app/dashboard.tsx
+++ components/app/dashboard.tsx
@@ -9,7 +9,7 @@ export const Dashboard = () => {
   return(
     <main className="w-full h-full flex-col space-y-2">
       <h1 className={cn(GeistSans.className, "text-5xl font-bold tracking-tight")}>DASHBOARD</h1>
-        <p className={cn("text-muted-foreground tracking-wide text-[14px]", GeistMono.className)}>// Edit your goals to start seeing changes</p>
+        <p className={cn("text-muted-foreground tracking-wide text-[14px]", GeistMono.className)}>{'//'} Edit your goals to start seeing changes</p>
       <div className="flex space-x-5">
         <PolyCard header="Pushups">
           0/100
