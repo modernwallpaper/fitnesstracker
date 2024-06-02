a2d91fc33d64f82a4f104191c06dfa0f42b79f99
modernwallpaper <j.cunz@grimmels.de>
2024-06-02 19:07:49 +0200

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { PolyCard } from "@/components/cool-card";
 
 export default function Home() {
   return (
-    <main className="p-11">
+    <main className="md:p-11 pt-2">
       <Dashboard />
     </main>
   );
--- components/app/dashboard.tsx
+++ components/app/dashboard.tsx
@@ -2,15 +2,17 @@ import { cn } from "@/lib/utils"
 import { GeistSans } from "geist/font/sans"
 import { PolyCard } from "../cool-card"
 import { GeistMono } from "geist/font/mono"
+import { Button } from "../ui/button"
+import { Plus } from "lucide-react"
 
 export const Dashboard = () => {
   return(
     <main className="w-full h-full flex-col space-y-2">
       <h1 className={cn(GeistSans.className, "text-5xl font-bold tracking-tight")}>DASHBOARD</h1>
-        <p className={cn("text-muted-foreground tracking-wide text-[14px]", GeistMono.className)}>// Edit your infos to start seeing changes</p>
+        <p className={cn("text-muted-foreground tracking-wide text-[14px]", GeistMono.className)}>// Edit your goals to start seeing changes</p>
       <div className="flex space-x-5">
         <PolyCard header="Pushups">
-          0/200
+          0/100
         </PolyCard>
         <PolyCard header="Bicep Curls">
           0/60
@@ -19,6 +21,10 @@ export const Dashboard = () => {
           0/60
         </PolyCard>
       </div>
+      <Button variant={"outline"} className="flex items-center space-x-2">
+        <Plus className="h-4 w-4"/>
+        <p>Add Goal</p>
+      </Button>
     </main>
   )
 }
