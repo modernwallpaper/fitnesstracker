10424016b9ea9ae6107cc02d3f45c0f32a1de821
modernwallpaper <j.cunz@grimmels.de>
2024-06-09 21:15:07 +0200

    Added comps

--- components/app/add-task.tsx
+++ components/app/add-task.tsx
@@ -1,9 +1,6 @@
 export const AddTask = () => {
   return(
     <div className="flex w-full p-12">
-      <div>
-        INFO: You can only create one card at a time
-      </div>
     </div>
   )
 }
