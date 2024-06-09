82e64e91932412ad0332d64b93a6dd5832471532
modernwallpaper <j.cunz@grimmels.de>
2024-06-09 19:34:19 +0200

    Add task form

---
+++ components/app/add-task.tsx
@@ -0,0 +1,9 @@
+export const AddTask = () => {
+  return(
+    <div className="flex w-full p-12">
+      <div>
+        INFO: You can only create one card at a time
+      </div>
+    </div>
+  )
+}
